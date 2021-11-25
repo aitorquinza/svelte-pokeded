a55d1192f2fd8bedb9ce2c4d7966aaf6f8172c62
Aitor Quinza <aitor@woosports.com>
2021-11-25 03:52:52 +0100

    Remove CLoudflare Workers

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,9 @@
 /** @type {import('@sveltejs/kit').Config} */
 
-import adapter from '@sveltejs/adapter-cloudflare-workers';
-
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#test',
-		adapter: adapter()
+		target: '#test'
 	}
 };
 
